612ed681db10796596cfa4dde91a6894fd0ea02e
samip.17 <dsamip11@gmail.com>
2022-05-15 18:18:17 +0530

    - clean up

--- lib/utils/base_strings.dart
+++ lib/utils/base_strings.dart
@@ -1,35 +1,10 @@
 class BaseStrings {
-  static const String title = 'title';
-  static const String updatedResume = 'updatedResume';
-  static const String resumeCollectionPath = 'extraResume';
   static const String internetNotAvailable = 'Internet not available';
   static const String errorOccurs = 'Error Occured';
 
-  ///landing
-  static const String forString = 'For';
-  static const String creators = 'creators';
-  static const String companies = 'companies';
-  static const String haveAccount = 'Already have an account?';
   static const String login = 'LOGIN';
-  static const String here = 'here';
 
   /// reg
-  static const String fName = 'First Name';
-  static const String lName = 'Last Name';
-  static const String gender = 'Gender';
-  static const String residence = 'Residence';
   static const String email = 'Email';
   static const String password = 'Password';
-  static const String cPassword = 'Confirm Password';
-  static const String instagram = 'Instagram Name';
-  static const String accept = 'I accept ';
-  static const String terms = 'Terms and Conditions';
-  static const String signUp = 'Sign Up';
-
-  ///login
-  static const String forgotPassword = 'Forgot your password?';
-
-  ///home
-  static const String search = 'Search';
-  static const String welcome = 'Welcome';
 }
